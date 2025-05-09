69222494c9728647b5806b2314aaa11660f017e5
Dhjali <barbara.vanni@laplateforme.io>
2025-05-09 11:43:45 +0200

    [ADD] react-project

---
+++ src/test/java/backend/overlook_hotel/OverlookHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.overlook_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlookHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
