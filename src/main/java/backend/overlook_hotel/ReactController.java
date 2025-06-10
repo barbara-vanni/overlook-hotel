7b5c48819669ed5212b00650b69e9b3a9df8940a
Dhjali <barbara.vanni@laplateforme.io>
2025-06-10 10:40:05 +0200

    [ADD] components & pages module in frontend/src // add Home.jsx for homePage

---
+++ src/main/java/backend/overlook_hotel/ReactController.java
@@ -0,0 +1,13 @@
+package backend.overlook_hotel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReactController {
+
+    @GetMapping("/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/frontend/index.html";
+    }
+}
