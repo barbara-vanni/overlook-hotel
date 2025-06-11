cd7e35b9e0322b8bf44b4a18d09f8a6b86ed5fa6
walidwade13 <walid.sek@laposte.net>
2025-06-11 19:51:11 +0200

    [CREATE] button logout added on the Header. localStorage= accessToken removed

--- frontend/src/components/HeaderModule/Header.tsx
+++ frontend/src/components/HeaderModule/Header.tsx
@@ -1,19 +1,37 @@
 import "./Header.css";
 import {AppBar} from "@mui/material";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
 
 const Header = () => {
+    const navigate = useNavigate();
+    const isAuthenticated = localStorage.getItem("accessToken");
+
+    const handleLogout = () => {
+        localStorage.removeItem("accessToken");
+        navigate("/login");
+    };
+
     return (
         <header className="header">
-            <AppBar  sx={{flexDirection: "row", padding:"0 50px" }} className="app-bar">
-            <h1>Overlook Hotel</h1>
-            <nav className="nav">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/reservations">Reservations</a></li>
-                    <li><a href="/rooms">Rooms</a></li>
-                    <li><a href="/guests">Guests</a></li>
-                </ul>
-            </nav>
+            <AppBar sx={{ flexDirection: "row", padding: "0 50px", alignItems: "center", gap: 4 }} className="app-bar">
+                <h1>Overlook Hotel</h1>
+                <nav className="nav">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/rooms">Rooms</a></li>
+                        <li><a href="/guests">Guests</a></li>
+                    </ul>
+                </nav>
+
+                {/* Afficher uniquement le bouton Déconnexion */}
+                {isAuthenticated && (
+                    <Button variant="outlined" color="inherit" onClick={handleLogout}>
+                        Déconnexion
+                    </Button>
+                )}
             </AppBar>
         </header>
     );
