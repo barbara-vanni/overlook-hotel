ff33ec7c0224d3b57df7c733bb7eda6785158d42
mathis-serra <mathis.serra@laplateforme.io>
2025-07-11 13:02:33 +0200

    Change the port in CorsConfig

--- src/main/java/backend/overlook_hotel/CorsConfig.java
+++ src/main/java/backend/overlook_hotel/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Applique à toutes les routes de l'API
-                .allowedOrigins("http://localhost:5174") // Autorise ton frontend React
+                .allowedOrigins("http://localhost:5173") // Autorise ton frontend React
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Autorise l’envoi de cookies/token si besoin
